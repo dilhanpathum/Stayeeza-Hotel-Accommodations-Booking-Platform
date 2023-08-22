d5733b116bc716d3bf3cd2319e640832651b8f9c
dilhanpathum <dilhan.jayasiri@gmail.com>
2023-08-22 10:37:34 +0530

    aall

---
+++ Classes/Booking.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Classes;
+
+/**
+ * Description of Booking
+ *
+ * @author dilha
+ */
+class Booking {
+    //put your code here
+}
---
+++ Classes/Feedback.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Classes;
+
+/**
+ * Description of Feedback
+ *
+ * @author dilha
+ */
+class Feedback {
+    //put your code here
+}
